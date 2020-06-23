46480f8a95404b31804bcf380b908852681def25
Lam Le <lam.le2@asiantech.vn>
2020-06-23 14:19:22 +0700

    Update UI and Fix code

---
+++ FinalProject/View/Controller/HomeController/CountryController/CountryViewController.swift
@@ -0,0 +1,86 @@
+//
+//  HomeCountryViewController.swift
+//  FinalProject
+//
+//  Created by PCI0002 on 6/19/20.
+//  Copyright © 2020 Thinh Nguyen X. All rights reserved.
+//
+
+import UIKit
+import SVProgressHUD
+
+final class CountryViewController: BaseViewController {
+
+    // MARK: - Properties
+    @IBOutlet weak var collectionView: UICollectionView!
+    var viewModel = CountryViewModel()
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+<<<<<<< Updated upstream
+
+    override func setUpUI() {
+        <#code#>
+    }
+    
+=======
+    override func setUpUI() {
+        <#code#>
+    }
+
+>>>>>>> Stashed changes
+    override func setUpData() {
+        registerColletionCell()
+    }
+
+    // MARK: Private Funtions
+    private func loadAPI() {
+        SVProgressHUD.show()
+        viewModel.getAPIListArea { (done, msg) in
+            SVProgressHUD.dismiss()
+            if done {
+                self.updateView()
+            } else {
+                self.showAlert(message: msg)
+            }
+        }
+        SVProgressHUD.setOffsetFromCenter(UIOffset(horizontal: UIScreen.main.bounds.width / 2, vertical: UIScreen.main.bounds.height / 2))
+    }
+
+    private func registerColletionCell() {
+        let nib = UINib(nibName: "CountryCollectionViewCell", bundle: .main)
+        collectionView.register(nib, forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    private func updateView() {
+        collectionView.reloadData()
+    }
+    
+    private func showAlert(message: String) {
+        let alert = UIAlertController(title: "Connect API", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Connect", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+}
+
+extension CountryViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.numberOfItemsInSection()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+<<<<<<< Updated upstream
+        return UICollectionViewCell()
+    }
+=======
+        <#code#>
+    }
+
+>>>>>>> Stashed changes
+}
