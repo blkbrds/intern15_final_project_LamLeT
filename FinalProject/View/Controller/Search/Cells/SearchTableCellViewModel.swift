ddf765c149aa19dd392687a60193a5cb58d05e58
Lam Le <lam.le2@asiantech.vn>
2020-07-09 14:57:55 +0700

    Create UI Search Screen

---
+++ FinalProject/View/Controller/Search/Cells/SearchTableCellViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  SearchTableCellViewModel.swift
+//  FinalProject
+//
+//  Created by PCI0002 on 7/9/20.
+//  Copyright © 2020 Thinh Nguyen X. All rights reserved.
+//
+
+import Foundation
+
